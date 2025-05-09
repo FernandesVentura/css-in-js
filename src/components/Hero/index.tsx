c31f0aa2147cdb1d1fc68a1a2629f7be88704408
Fernandes Ventura <fernandes.ventura25@gmail.com>
2025-05-09 19:08:21 +0100

    initial commit

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Form, FormDiv, HeroTitle } from './Hero.module'
+
+const Hero = () => (
+  <Form >
+    <FormDiv className="container">
+      <HeroTitle >
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </FormDiv>
+  </Form>
+)
+
+export default Hero
---
+++ src/components/Vaga/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { VagaLi, VagaLink, VagaTitulo } from './Vaga.module'
+
+type Props = {
+  titulo: string
+  localizacao: string
+  nivel: string
+  modalidade: string
+  salarioMin: number
+  salarioMax: number
+  requisitos: string[]
+}
+
+const Vaga = (props: Props) => (
+  <VagaLi >
+    <VagaTitulo >{props.titulo}</VagaTitulo>
+    <ul>
+      <li>Localizacao: {props.localizacao}</li>
+      <li>Senioridade: {props.nivel}</li>
+      <li>Tipo de contratacao: {props.modalidade}</li>
+      <li>
+        Salário: {props.salarioMin} - {props.salarioMax}
+      </li>
+      <li>Requisitos: {props.requisitos.join(', ')}</li>
+    </ul>
+    <VagaLink  href="#">
+      Ver detalhes e candidatar-se
+    </VagaLink>
+  </VagaLi>
+)
+
+export default Vaga
